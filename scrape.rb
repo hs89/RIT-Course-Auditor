5a11908b8910636921d5db196eb23a11efc93171
hs89 <hsandlar@gmail.com>
2012-03-16 11:16:47 -0400

    Structure in place to implement redis store

--- scrape.rb
+++ scrape.rb
@@ -1,9 +1,6 @@
 require 'mechanize'
 require 'highline'
-
-agent = Mechanize.new
-agent.user_agent_alias = 'Windows Mozilla'
-agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+require 'redis'
 
 class Course
   def init(course_num, course_title, course_instructor, course_days, course_from, course_to, course_location)
@@ -12,10 +9,9 @@ class Course
     @course_instructor = course_instructor
 
     #TODO: Fix this regex.. it will properly create the list of professors teaching the course but if they have a trailing Jr. in their name it doesnt work    
-    matches = @course_instructor.scan(/(\S{1}\s\S{1+})/)
+    matches = @course_instructor.scan(/(\S\s\S+)/)
     if(matches.length > 1)
       @course_instructor = "#{matches[0, matches.length].join(', ')}"
-      puts @course_instructor
     else
     end
     @course_days = course_days
@@ -26,10 +22,13 @@ class Course
   def print
     puts @course_num + "  " + @course_title + "  " + @course_instructor + "  " + @course_days + "  " + @course_from + "  " + @course_to + "  " + @course_location
   end
+  def write
+    print
+  end
 end
 
 
-def mine_data(discipline_number, course_list_page)
+def create_database(discipline_number, course_list_page)
   course_table_rows = course_list_page.parser.xpath("//table[@class='stripeMe courseList menuBox']/tbody/tr")
   course_table_rows.collect do |row|
     temp_course = Course.new
@@ -44,8 +43,7 @@ def mine_data(discipline_number, course_list_page)
       @course_from = row.at("td[3]").text.strip
       @course_to = row.at("td[4]").text.strip
       @course_location = row.at("td[5]").text.strip     
-      temp_course.init(@course_num, @course_title, @course_instructor, @course_days, @course_from, @course_to, @course_location)
-      #temp_course.print    
+      temp_course.init(@course_num, @course_title, @course_instructor, @course_days, @course_from, @course_to, @course_location) 
     else
       @course_num = discipline_number.to_s + "-" + row.at("td[1]").text.strip
       @course_title = row.at("td[2]").text.strip
@@ -56,15 +54,32 @@ def mine_data(discipline_number, course_list_page)
       @course_location = row.at("td[10]").text.strip
       temp_course.init(@course_num, @course_title, @course_instructor, @course_days, @course_from, @course_to, @course_location)      
     end
+    temp_course.write
   end
 end
 
-discipline_selection_page = agent.get "https://sis.rit.edu/info/info.do?init=openCourses"
-discipline_form = discipline_selection_page.form_with(:action => "getOpenCourseList.do?init=openCourses")
-discipline_dropdown = discipline_form.field_with(:name => "discipline")
+def scrape_SIS
+  agent = Mechanize.new
+  agent.user_agent_alias = 'Windows Mozilla'
+  agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
 
-discipline_dropdown.options.each do |opt|
-  discipline_dropdown.value = opt
-  discipline_course_page = discipline_form.submit
-  mine_data(opt, discipline_course_page)  
+  discipline_selection_page = agent.get "https://sis.rit.edu/info/info.do?init=openCourses"
+  discipline_form = discipline_selection_page.form_with(:action => "getOpenCourseList.do?init=openCourses")
+  discipline_dropdown = discipline_form.field_with(:name => "discipline")
+  
+  discipline_dropdown.options.each do |opt|
+    discipline_dropdown.value = opt
+    discipline_course_page = discipline_form.submit
+    create_database(opt, discipline_course_page)  
+  end
+end
+
+def connect_database
+  
+end
+
+if(connect_database)
+  scrape_SIS
+else
+  puts "Failed to connect to the database"
 end
