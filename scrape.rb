bf199a7ce257d2dae12dafdb81c5fba0f8ef8c28
hs89 <hsandlar@gmail.com>
2012-03-16 16:29:32 -0400

    somewhat sloppy implementation of piping data into redis but it works

--- scrape.rb
+++ scrape.rb
@@ -1,10 +1,55 @@
 require 'mechanize'
 require 'highline'
 require 'redis'
+require 'json'
 
 @database_server = "96.8.118.7"
 @database_port = 1234
 @database_password ="audit"
+
+@monday = Redis.new(:host => @database_server,
+                   :port => @database_port)
+@tuesday = Redis.new(:host => @database_server,
+                   :port => @database_port)
+@wednesday = Redis.new(:host => @database_server,
+                   :port => @database_port)
+@thursday = Redis.new(:host => @database_server,
+                   :port => @database_port)
+@friday = Redis.new(:host => @database_server,
+                   :port => @database_port)
+@saturday = Redis.new(:host => @database_server,
+                   :port => @database_port)
+@sunday = Redis.new(:host => @database_server,
+                   :port => @database_port)
+@course_db = Redis.new(:host => @database_server,
+                   :port => @database_port)
+
+def connect_database
+
+  connected = @monday.auth(@database_password)
+  @monday.select(0)
+  @tuesday.auth(@database_password)
+  @tuesday.select(1)
+  @wednesday.auth(@database_password)
+  @wednesday.select(2)
+  @thursday.auth(@database_password)
+  @thursday.select(3)
+  @friday.auth(@database_password)
+  @friday.select(4)
+  @saturday.auth(@database_password)
+  @saturday.select(5)
+  @sunday.auth(@database_password)
+  @sunday.select(6)
+  @course_db.auth(@database_password)
+  @course_db.select(7)
+  
+  
+  if (connected == "OK")
+    then return true
+  else
+    return false
+  end
+end
 
 class Course
   def init(course_num, course_title, course_instructor, course_days, course_from, course_to, course_location)
@@ -36,7 +81,28 @@ class Course
     puts @course_num + "  " + @course_title + "  " + @course_instructor + "  " + @course_days + "  " + @course_from + "  " + @course_to + "  " + @course_location
   end
   
-  def write
+  def convertTimeToMilitary(time)
+    if (time == "800AM") then return 800
+    elsif (time == "900AM") then return 900
+    elsif (time == "1000AM") then return 1000
+    elsif (time == "1100AM") then return 1100
+    elsif (time == "1200N") then return 1200
+    elsif (time == "100PM") then return 1300
+    elsif (time == "200PM") then return 1400
+    elsif (time == "300PM") then return 1500
+    elsif (time == "400PM") then return 1600
+    elsif (time == "500PM") then return 1700
+    elsif (time == "600PM") then return 1800
+    elsif (time == "700PM") then return 1900
+    elsif (time == "800PM") then return 2000
+    elsif (time == "900PM") then return 2100
+    else
+      return 2400
+    end
+    
+  end
+  
+  def write(monday,tuesday,wednesday,thursday,friday,saturday,sunday,course_db)
     if(@course_location == "TBA" ||
        @course_days == "TBA" ||
        @course_from == "TBA" ||
@@ -46,7 +112,35 @@ class Course
       return
     else
       #Write Data to appropriate databases
-      print
+      course_from = convertTimeToMilitary(@course_from)
+      @course_location.match(/(.{3})-*/)
+      bldg_num = $1
+      if(@course_days =~ /M/)
+        monday.zadd(bldg_num, course_from, @course_num)
+      if(@course_days =~ /T/)
+        tuesday.zadd(bldg_num, course_from, @course_num)
+      if(@course_days =~ /W/)
+        wednesday.zadd(bldg_num, course_from, @course_num)
+      if(@course_days =~ /R/)
+        thursday.zadd(bldg_num, course_from, @course_num)
+      if(@course_days =~ /F/)
+        friday.zadd(bldg_num, course_from, @course_num)
+      if(@course_days =~ /S/)
+        saturday.zadd(bldg_num, course_from, @course_num)
+      if(@course_days =~ /U/)
+        sunday.zadd(bldg_num, course_from, @course_num)
+      end
+      
+      json_obj = {
+        "num" => @course_num,
+        "title" => @course_title,
+        "instructor" => @course_instructor,
+        "days" => @course_days,
+        "from" => @course_from,
+        "to" => @course_to,
+        "location" => @course_location
+      }.to_json
+      course_db.set(@course_num,json_obj)
     end    
   end
 end
@@ -79,7 +173,7 @@ def create_database(discipline_number, course_list_page)
       temp_course.init(@course_num, @course_title, @course_instructor, @course_days, @course_from, @course_to, @course_location)      
     end
     if(temp_course != false)
-      temp_course.write    
+      temp_course.write(@monday,@tuesday,@wednesday,@thursday,@friday,@saturday,@sunday,@course_db)   
     end
   end
 end
@@ -97,18 +191,6 @@ def scrape_SIS
     discipline_dropdown.value = opt
     discipline_course_page = discipline_form.submit
     create_database(opt, discipline_course_page)  
-  end
-end
-
-def connect_database
-  @redis = Redis.new(:host => @database_server,
-                     :port => @database_port)
-  connected = @redis.auth(@database_password)
-  
-  if (connected == "OK")
-    then return true
-  else
-    return false
   end
 end
 
